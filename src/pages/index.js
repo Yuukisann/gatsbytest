aaef5f846db69718f12a3899cb52e3f2b23c9216
yuukisann1 <yuki-iphone7abcd@ezweb.ne.jp>
2020-09-06 21:04:09 +0900

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div>
+    <h1>hello world</h1>
+    <p>hello </p>
+    <Link to="/contact">go contact</Link>
+  </div>
+  )
 }
